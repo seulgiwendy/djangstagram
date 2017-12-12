ee694f12ee07cbc11faace4e6d923fa02ab799d3
seulgiwendy <wheejuni@gmail.com>
2017-12-12 11:55:59 +0900

    added README.md

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Accounts, Comments, Photos
+
+admin.site.register(Accounts)
+admin.site.register(Comments)
+admin.site.register(Photos)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Accounts(models.Model):
+    name = models.CharField(max_length=24)
+    password = models.CharField(max_length=24)
+    join_date = models.DateTimeField()
+
+    def join(self):
+        self.join_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return "%s, joined : %s" % (self.name, self.join_date)
+
+
+class Photos(models.Model):
+    description = models.CharField(max_length=144)
+    like_count = models.BigIntegerField()
+    post_date = models.DateTimeField()
+    author = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+
+class Comments(models.Model):
+    content = models.CharField(max_length=144)
+    author = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photos, on_delete=models.CASCADE)
+
+
+
